a89cf6d6aede70a4698df6b389904de1334143e4
mpizurica <pizurica.marija@gmail.com>
2022-09-11 12:43:55 -0700

    updates

--- src/components/MainInfoBanner.vue
+++ src/components/MainInfoBanner.vue
@@ -4,6 +4,6 @@
     </button> -->
     <div class="p-5 border-2 border-emerald-300 bg-emerald-50 rounded-xl hover:shadow-lg duration-500">
         <h3 class="text-emerald-800 pb-2 uppercase font-bold">Internship opportunities: </h3>
-        <p class="text-emerald-800"> I am looking for internship opportunities for summer 2023, in the area of medical imaging and Deep Learning. Feel free to contact me with any leads!</p>
+        <p class="text-emerald-800"> I am looking for DL/ML internship opportunities for Summer 2023. Feel free to contact me with any leads!</p>
     </div>
 </template>
--- src/components/PublicationList.vue
+++ src/components/PublicationList.vue
@@ -3,40 +3,46 @@
     <div class="flex flex-col space-y-5">
         <div class="grid grid-cols-5">
             <div>
-                <p class="list-info">ICML `22</p>
+                <p class="list-info"> [in preparation]</p>
             </div>
             <div class="col-span-4">
-                <p>Self-supervised multimodel pre-training for lung adenocarcinoma overall survival prediction <button class="link">[arXiv]</button> 
+                <p>WSI based prediction of <span class='italic'> TP53</span> mutations identifies aggressive disease phenotype in prostate cancer <button class="link">[arXiv]</button>
                     <button class="link">[code]</button><button class="link">[cite]</button></p>
-                <p class="pt-1 text-xs">Mathieu De Coster, Karel D'Oosterlinck, Marija Pizurica, Paloma Rabaey, Severine
-                    Verlinden, Mieke Van Herreweghe, Joni Dambre</p>
-                <div class="h-40 mt-2">
-                    <img class="h-full w-full shadow-lg object-cover rounded-xl" src="../assets/marija_cropped.jpeg" alt="">
+                <p class="pt-1 text-xs">Marija Pizurica, Maarten Larmuseau, Kim Van der Eecken, Francisco Carrillo-Perez,
+                     Louise de Schaetzen van Brienen, Nicolaas Lumen, Jo Van Dorpe, Piet Ost, Sofie Verbeke, Olivier Gevaert<sup>*</sup> 
+                     and Kathleen Marchal<sup>*</sup></p>
+                <div class="h-55 mt-2">
+                    <img class="h-full w-full shadow-md object-cover rounded-xl" src="../assets/WSI_mut.jpg" alt="">
                 </div>
             </div>
         </div>
         <div class="grid grid-cols-5">
             <div>
-                <p class="list-info">ICML `22</p>
+                <p class="list-info">BIOCOMP `22</p>
             </div>
             <div class="col-span-4">
-                <p>Self-supervised multimodel pre-training for lung adenocarcinoma overall survival prediction <button class="link">[arXiv]</button>
+                <p>Self-supervised multimodel pre-training for lung adenocarcinoma overall survival prediction <button class="link">[arXiv]</button> 
                     <button class="link">[code]</button><button class="link">[cite]</button></p>
-                <p class="pt-1 text-xs">Mathieu De Coster, Karel D'Oosterlinck, Marija Pizurica, Paloma Rabaey, Severine
-                    Verlinden, Mieke Van Herreweghe, Joni Dambre</p>
+                <p class="pt-1 text-xs">Francisco Carrillo-Perez<sup>*</sup>, Marija Pizurica<sup>*</sup>, Ignacio Rojas, Kathleen Marchal, 
+                                        Luis Javier Herrera and Olivier Gevaert
+                </p>
+                <div class="h-55 mt-2">
+                    <img class="h-full w-full shadow-md object-cover rounded-xl" src="../assets/SSL_im.jpg" alt="">
+                </div>
             </div>
         </div>
         <div class="grid grid-cols-5">
             <div>
-                <p class="list-info">ICML `22</p>
+                <p class="list-info">AT4SSL `21</p>
             </div>
             <div class="col-span-4">
-                <p>Self-supervised multimodel pre-training for lung adenocarcinoma overall survival prediction <button class="link">[arXiv]</button>
+                <p>Frozen Pretrained Transformers for Neural Sign Language Translation <button class="link">[arXiv]</button>
                     <button class="link">[code]</button><button class="link">[cite]</button></p>
                 <p class="pt-1 text-xs">Mathieu De Coster, Karel D'Oosterlinck, Marija Pizurica, Paloma Rabaey, Severine
                     Verlinden, Mieke Van Herreweghe, Joni Dambre</p>
             </div>
         </div>
+        
         <div class="grid grid-cols-5">
             <div>
                 <p class="list-info">ICML `22</p>
